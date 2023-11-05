e7079df574e3500c8f96c6f0af1838ee1ec5f2ff
Cameron Dodd <cadodd@ucsc.edu>
2023-11-05 15:00:06 -0800

    Unique coins and centered around null island

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,32 @@ import leaflet, { LatLng } from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
 
-const MERRILL_CLASSROOM = leaflet.latLng({
-  lat: 36.9995,
-  lng: -122.0533,
-});
+const NULL_ISLAND = leaflet.latLng(0, 0);
 
 const GAMEPLAY_ZOOM_LEVEL = 18.5;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const PIT_SPAWN_PROBABILITY = 0.1;
+const COIN_RATE_MOD = 100;
+
+class Coin {
+  public i: number;
+  public j: number;
+  public index: number;
+  constructor(i: number, j: number, index: number) {
+    this.i = i;
+    this.j = j;
+    this.index = index;
+  }
+}
+
+const inventory: Coin[] = [];
+const pitData: Map<string, Coin[]> = new Map<string, Coin[]>();
 
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
 const map = leaflet.map(mapContainer, {
-  center: MERRILL_CLASSROOM,
+  center: NULL_ISLAND,
   zoom: GAMEPLAY_ZOOM_LEVEL,
   minZoom: GAMEPLAY_ZOOM_LEVEL,
   maxZoom: GAMEPLAY_ZOOM_LEVEL,
@@ -33,7 +45,7 @@ leaflet
   })
   .addTo(map);
 
-const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
+const playerMarker = leaflet.marker(NULL_ISLAND);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
 
@@ -49,20 +61,14 @@ sensorButton.addEventListener("click", () => {
   });
 });
 
-let points = 0;
+//let points = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
 function makePit(i: number, j: number) {
   const bounds = leaflet.latLngBounds([
-    [
-      MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
-    ],
-    [
-      MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
-    ],
+    [i * TILE_DEGREES, j * TILE_DEGREES],
+    [(i + 1) * TILE_DEGREES, (j + 1) * TILE_DEGREES],
   ]);
 
   const pit = leaflet.rectangle(bounds) as leaflet.Layer;
@@ -71,56 +77,80 @@ function makePit(i: number, j: number) {
   pit.addTo(map);
 }
 
-const pitData: Map<string, number> = new Map<string, number>();
-
-function getPitValue(pitKey: string) {
+function getPitStorage(i: number, j: number) {
+  const pitKey = `${i},${j}`;
   if (!pitData.has(pitKey)) {
     // If the pit value is not in the lookup table, calculate and store it
-    const calculatedValue = Math.floor(luck([pitKey].toString()) * 100);
-    pitData.set(pitKey, calculatedValue);
-    return calculatedValue;
+    const coins = [];
+    const numCoins = Math.floor(luck([pitKey].toString()) * COIN_RATE_MOD);
+    for (let index = 0; index < numCoins; index++) {
+      coins.push(new Coin(i, j, index));
+    }
+    pitData.set(pitKey, coins);
+    return coins;
   }
   return pitData.get(pitKey)!;
 }
 
 function createPopup(i: number, j: number) {
-  let value = getPitValue(`${i},${j}`);
+  const coins = getPitStorage(i, j);
   const container = document.createElement("div");
   container.innerHTML = `
-            <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-            <button id="Take">Take coin</button><button id="Drop">Drop coin</button>`;
-  const take = container.querySelector<HTMLButtonElement>("#Take")!;
-  take.addEventListener("click", () => {
-    if (value == 0) {
-      return;
-    }
-    value--;
-    container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-      value.toString();
-    points++;
-    statusPanel.innerHTML = `${points} points accumulated`;
-  });
-  const drop = container.querySelector<HTMLButtonElement>("#Drop")!;
-  drop.addEventListener("click", () => {
-    if (points == 0) {
-      return;
-    }
-    value++;
-    container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-      value.toString();
-    points--;
-    statusPanel.innerHTML = `${points} points accumulated`;
-  });
+            <div>There is a cache here at "${i},${j}".
+            It has <span id="value">${coins.length}</span> coin${
+    coins.length != 1 ? "s" : ""
+  } in it.</div>
+            <button id="Drop">Leave a coin</button>`;
+
+  for (const coin of coins) {
+    const coinDiv = document.createElement("div");
+    coinDiv.innerHTML = `
+      Coin:${coin.i},${coin.j},${coin.index}
+      <button id="Take">Take coin</button>`;
+    const take = coinDiv.querySelector<HTMLButtonElement>("#Take")!;
+    take.addEventListener("click", () => {
+      inventory.push(coin);
+      coins.splice(coins.indexOf(coin), 1);
+      container.querySelector<HTMLSpanElement>(
+        "#value"
+      )!.innerHTML = `${coins.length}`;
+      statusPanel.innerHTML = `${inventory.length} points accumulated`;
+      coinDiv.remove();
+    });
+    container.append(coinDiv);
+  }
+  // const take = container.querySelector<HTMLButtonElement>("#Take")!;
+  // take.addEventListener("click", () => {
+  //   if (value == 0) {
+  //     return;
+  //   }
+  //   value--;
+  //   container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+  //     value.toString();
+  //   points++;
+  //   statusPanel.innerHTML = `${points} points accumulated`;
+  // });
+  // const drop = container.querySelector<HTMLButtonElement>("#Drop")!;
+  // drop.addEventListener("click", () => {
+  //   if (points == 0) {
+  //     return;
+  //   }
+  //   value++;
+  //   container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+  //     value.toString();
+  //   points--;
+  //   statusPanel.innerHTML = `${points} points accumulated`;
+  // });
   return container;
 }
 
-spawnPits(MERRILL_CLASSROOM);
+spawnPits(NULL_ISLAND);
 function spawnPits(position: LatLng) {
   const latitude = position.lat;
   const longitude = position.lng;
 
-  const i = Math.floor((latitude - MERRILL_CLASSROOM.lat) / TILE_DEGREES);
-  const j = Math.floor((longitude - MERRILL_CLASSROOM.lng) / TILE_DEGREES);
+  const i = Math.floor(latitude / TILE_DEGREES);
+  const j = Math.floor(longitude / TILE_DEGREES);
 
   for (let deltaI = -NEIGHBORHOOD_SIZE; deltaI < NEIGHBORHOOD_SIZE; deltaI++) {
     for (
